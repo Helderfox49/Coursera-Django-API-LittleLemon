91c037165ca7248cdc52c4558328f1bcf08c4ec2
Helderfox49 <mbottoguyzo2012@gmail.com>
2025-07-21 15:39:19 +0100

    End Module 2

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -6,11 +6,17 @@ from .serializers import MenuItemSerializer
 from django.shortcuts import get_object_or_404
 
 
-@api_view()
+@api_view(['GET', 'POST'])
 def menu_items(request):
-    items = MenuItem.objects.all()
-    serializer_items = MenuItemSerializer(items, many=True)
-    return Response(serializer_items.data)
+    if request.method == 'GET':
+        items = MenuItem.objects.all()
+        serializer_items = MenuItemSerializer(items, many=True)
+        return Response(serializer_items.data)
+    if request.method == 'POST':
+        serializer_item = MenuItemSerializer(data=request.data)
+        serializer_item.is_valid(raise_exception=True)
+        serializer_item.save()
+        return Response(serializer_item.data)
 
 @api_view()
 def single_item(request, id):
